c02691ddf7db3dc2e62fd06691b5fe1e02347d5a
kbsssudha <kbssudha@gmail.com>
2024-01-06 20:20:15 +0530

    Calculator

---
+++ day1/Operators.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class Operators {
+    public static void main(String[] args) {
+        String name="Sudhasundari. K";
+        int age= 30;
+        long adhar= 3309256799988L;
+        short vno= 5251;
+        float weight=75.45f;
+        double rate=134656.34444;
+        char ch='S';
+        boolean result=true;
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Adhar No: "+adhar);
+        System.out.println("Vehicle No:"+vno);
+        System.out.println("Weight: "+weight);
+        System.out.println("Rate: "+rate);
+        System.out.println("Result: "+ch);
+        System.out.println("Final Result: "+result);
+
+    }
+}
