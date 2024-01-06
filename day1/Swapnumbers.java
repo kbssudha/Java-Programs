7a366f4848109c069eedc6923daa2215912df45c
kbssudha <kbssudha@gmail.com>
2024-01-06 20:57:14 +0530

    Swap two numbers

---
+++ day1/Swapnumbers.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Swapnumbers {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        System.out.println("Before swapping : number-1 = "+a+"; number-2 = "+b);
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println("After swapping : number-1 = "+a+ "; number-2 = "+b);
+    }
+}
