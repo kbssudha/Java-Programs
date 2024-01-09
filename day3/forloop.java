35b9ba6d5242026eb0ce058ee25ed95d3f545cb6
kbssudha <kbssudha@gmail.com>
2024-01-09 22:28:24 +0530

    forloop to print no 1 to 100

---
+++ day3/forloop.java
@@ -0,0 +1,10 @@
+package day3;
+
+public class forloop {
+    public static void main(String[] args) {
+      //  int n = 0;
+        for (int i = 1; i <=100; i++) {
+            System.out.println(i);
+        }
+    }
+}
